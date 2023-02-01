39cfe485cd61422919877ed193e5c456584b9f01
risto.heinsar <risto.heinsar@ICT-50100K>
2023-02-01 16:43:53 +0200

    Added clock with a timer and menu strip

--- PrjConverterForm/FormConverter.Designer.vb
+++ PrjConverterForm/FormConverter.Designer.vb
@@ -22,16 +22,23 @@ Partial Class FormConverter
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btnConvert = New System.Windows.Forms.Button()
         Me.txtKilometers = New System.Windows.Forms.TextBox()
         Me.txtMiles = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.timerClock = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenüüToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SulgeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnConvert
         '
-        Me.btnConvert.Location = New System.Drawing.Point(200, 61)
+        Me.btnConvert.Location = New System.Drawing.Point(153, 56)
         Me.btnConvert.Name = "btnConvert"
         Me.btnConvert.Size = New System.Drawing.Size(198, 28)
         Me.btnConvert.TabIndex = 0
@@ -40,14 +47,14 @@ Partial Class FormConverter
         '
         'txtKilometers
         '
-        Me.txtKilometers.Location = New System.Drawing.Point(200, 35)
+        Me.txtKilometers.Location = New System.Drawing.Point(153, 30)
         Me.txtKilometers.Name = "txtKilometers"
         Me.txtKilometers.Size = New System.Drawing.Size(198, 20)
         Me.txtKilometers.TabIndex = 1
         '
         'txtMiles
         '
-        Me.txtMiles.Location = New System.Drawing.Point(200, 95)
+        Me.txtMiles.Location = New System.Drawing.Point(153, 90)
         Me.txtMiles.Name = "txtMiles"
         Me.txtMiles.Size = New System.Drawing.Size(198, 20)
         Me.txtMiles.TabIndex = 2
@@ -55,7 +62,7 @@ Partial Class FormConverter
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(97, 38)
+        Me.Label1.Location = New System.Drawing.Point(50, 33)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(97, 13)
         Me.Label1.TabIndex = 3
@@ -64,24 +71,63 @@ Partial Class FormConverter
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(116, 98)
+        Me.Label2.Location = New System.Drawing.Point(69, 93)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(78, 13)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Vastus miilides:"
         '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Location = New System.Drawing.Point(181, 145)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(48, 13)
+        Me.lblTime.TabIndex = 5
+        Me.lblTime.Text = "Kellaaeg"
+        '
+        'timerClock
+        '
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenüüToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(424, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenüüToolStripMenuItem
+        '
+        Me.MenüüToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SulgeToolStripMenuItem})
+        Me.MenüüToolStripMenuItem.Name = "MenüüToolStripMenuItem"
+        Me.MenüüToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.MenüüToolStripMenuItem.Text = "Menüü"
+        '
+        'SulgeToolStripMenuItem
+        '
+        Me.SulgeToolStripMenuItem.Name = "SulgeToolStripMenuItem"
+        Me.SulgeToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SulgeToolStripMenuItem.Text = "Sulge"
+        '
         'FormConverter
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(519, 199)
+        Me.ClientSize = New System.Drawing.Size(424, 199)
+        Me.Controls.Add(Me.lblTime)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtMiles)
         Me.Controls.Add(Me.txtKilometers)
         Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "FormConverter"
         Me.Text = "Distantsi teisendaja"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -92,4 +138,9 @@ Partial Class FormConverter
     Friend WithEvents txtMiles As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents lblTime As Label
+    Friend WithEvents timerClock As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenüüToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SulgeToolStripMenuItem As ToolStripMenuItem
 End Class
--- PrjConverterForm/FormConverter.vb
+++ PrjConverterForm/FormConverter.vb
@@ -6,7 +6,7 @@
 
         Try
             Dim result = converter.ConvertKilometersToMiles(txtKilometers.Text)
-            txtMiles.Text = result
+            txtMiles.Text = Math.Round(result, 3)
         Catch ex As Exception
             txtMiles.Text = "Sisesta vaid numbrid!"
         End Try
@@ -15,5 +15,21 @@
 
     Private Sub FormConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         txtMiles.ReadOnly = True
+        timerClock.Enabled = True
+
+        ' timer ticks once a second
+        timerClock.Interval = 1000
+    End Sub
+
+    Private Sub timerClock_Tick(sender As Object, e As EventArgs) Handles timerClock.Tick
+        lblTime.Text = DateTime.Now
+    End Sub
+
+    Private Sub lblTime_Click(sender As Object, e As EventArgs) Handles lblTime.Click
+        timerClock.Enabled = Not timerClock.Enabled
+    End Sub
+
+    Private Sub SulgeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SulgeToolStripMenuItem.Click
+        Me.Close()
     End Sub
 End Class
