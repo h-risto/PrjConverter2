82aa79de6649c31d31b96615a9ae75723ff0826e
risto.heinsar <risto.heinsar@ICT-50100K>
2023-02-01 16:02:59 +0200

    Initial design complete

--- PrjConverterForm/FormConverter.vb
+++ PrjConverterForm/FormConverter.vb
@@ -3,6 +3,18 @@
     Private Const KM2MI = 1.609344
 
     Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
-        txtMiles.Text = txtKilometers.Text / KM2MI
+
+        Try
+            txtMiles.Text = txtKilometers.Text / KM2MI
+        Catch ex As Exception
+            ' MsgBox("Vigane sisestus!", MsgBoxStyle.Critical)
+            txtMiles.Text = "Vigane sisestus!!!!!"
+        End Try
+
+
+    End Sub
+
+    Private Sub FormConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtMiles.ReadOnly = True
     End Sub
 End Class
