b8a1e0bd109e75846bf3a2e0ec54a79eff339fa8
risto.heinsar <risto.heinsar@ICT-50100K>
2023-02-01 15:41:38 +0200

    Initial form layout

--- PrjConverterForm/My Project/Application.Designer.vb
+++ PrjConverterForm/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.PrjConverterForm.Form1
+            Me.MainForm = Global.PrjConverterForm.FormConverter
         End Sub
     End Class
 End Namespace
